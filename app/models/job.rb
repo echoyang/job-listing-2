f1a7b65ba5c68ba16fa74f519c20fdaf8703a9ad
yangzhujun <yangzhujun1@gmail.com>
2017-01-16 14:51:04 +0800

    app/models/job

--- app/models/job.rb
+++ app/models/job.rb
@@ -2,6 +2,7 @@ class Job < ApplicationRecord
   has_many :resumes
   validates :title, presence: true
   validates :wage_upper_bound, presence: true
+  validates :wage_lower_bound, presence: true
   validates :wage_lower_bound, numericality: { greater_than: 0}
   def publish!
     self.is_hidden = false
